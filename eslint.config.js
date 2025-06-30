21e861370b3c4a0f31787f18ab51df5bfa86ed67
Pablo <pcolturi@proton.me>
2025-06-30 09:14:47 +0200

    add eslint config for svelte files

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,39 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
+import js from "@eslint/js"
+import { defineConfig } from "eslint/config"
+import svelte from "eslint-plugin-svelte"
+import globals from "globals"
+import svelteConfig from './svelte.config.js'
 
 
 export default defineConfig([
   js.configs.recommended,
+  ...svelte.configs.recommended,
   { 
     ignores: ["out/**", "dist/**"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.js'],
+    languageOptions: {
+      parserOptions: {
+        // We recommend importing and specifying svelte.config.js.
+        // By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
+        // While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
+        // explicitly specifying it ensures better compatibility and functionality.
+        svelteConfig
+      }
+    }
   },
   {
     rules: {
       "semi": ["error", "never"],
     },
   },
-  { 
-    files: ["src/**/*.{js,mjs,cjs}"],
-    // ignores: ["electron/**"],
-    languageOptions: { globals: globals.browser },
-    // rules: {
-    //   "semi": ["error", "never"],
-    // },
-  },
-  { 
-    files: ["electron/**/*.{js,mjs,cjs}"],
-    // ignores: ["src/**"],
-    languageOptions: { globals: globals.node },
-    // rules: {
-    //   "semi": ["error", "never"],
-    // },
-  },
-]);
+])
