778762dcfb1c8ad020cb840676c5d1722d0aa563
Pablo <pcolturi@proton.me>
2025-07-21 17:13:13 +0200

    format

--- src/lib/utils/login.js
+++ src/lib/utils/login.js
@@ -10,23 +10,26 @@ export const login = async () => {
     // console.log('json', json)
 
     // test POST call and login authentication with remote server
-    const formData = new URLSearchParams();
-    formData.append("grant_type", "password");
-    formData.append("username", "admin@example.com");
-    formData.append("password", "changethis");
-    formData.append("scope", "");
-    formData.append("client_id", "string");
-    formData.append("client_secret", "string");
+    const formData = new URLSearchParams()
+    formData.append('grant_type', 'password')
+    formData.append('username', 'admin@example.com')
+    formData.append('password', 'changethis')
+    formData.append('scope', '')
+    formData.append('client_id', 'string')
+    formData.append('client_secret', 'string')
 
-    const myHeaders = new Headers();
-    myHeaders.append("accept", "application/json");
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    const myHeaders = new Headers()
+    myHeaders.append('accept', 'application/json')
+    myHeaders.append('Content-Type', 'application/x-www-form-urlencoded')
 
-    const response = await fetch("http://localhost:8000/api/v1/login/access-token", {
-      method: "POST",
-      headers: myHeaders,
-      body: formData.toString(),
-    })
+    const response = await fetch(
+      'http://localhost:8000/api/v1/login/access-token',
+      {
+        method: 'POST',
+        headers: myHeaders,
+        body: formData.toString(),
+      }
+    )
     const result = await response.json()
     console.log('result', result)
   } catch (error) {
