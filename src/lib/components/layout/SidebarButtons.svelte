9de133847dddf93114e48669efef9088c737e692
Pablo Colturi <pcolturi@proton.me>
2025-08-27 14:24:50 +0200

    #68 make svg icons more uniform

--- src/lib/components/layout/SidebarButtons.svelte
+++ src/lib/components/layout/SidebarButtons.svelte
@@ -111,11 +111,15 @@
       >
         <path
           d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z"
-          fill="var(--ternary-color)"
+          fill="none"
+          stroke="var(--ternary-color)"
+          stroke-width="1"
         />
         <path
           d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z"
-          fill="var(--ternary-color)"
+          fill="none"
+          stroke="var(--ternary-color)"
+          stroke-width="1"
         />
       </svg>
     </label>
@@ -208,14 +212,14 @@
         <path
           d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15"
           stroke="var(--ternary-color)"
-          stroke-width="2"
+          stroke-width="1.5"
           stroke-linecap="round"
           stroke-linejoin="round"
         />
         <path
           d="M12 16V3M12 3L16 7.375M12 3L8 7.375"
           stroke="var(--ternary-color)"
-          stroke-width="2"
+          stroke-width="1.5"
           stroke-linecap="round"
           stroke-linejoin="round"
         />
@@ -236,21 +240,23 @@
       title="Import grpah from JSON file"
     >
       <svg
-        width="30px"
-        height="30px"
+        width="25px"
+        height="25px"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M12 14L11.2929 14.7071L12 15.4142L12.7071 14.7071L12 14ZM13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44771 11 5L13 5ZM6.29289 9.70711L11.2929 14.7071L12.7071 13.2929L7.70711 8.29289L6.29289 9.70711ZM12.7071 14.7071L17.7071 9.70711L16.2929 8.29289L11.2929 13.2929L12.7071 14.7071ZM13 14L13 5L11 5L11 14L13 14Z"
-          fill="var(--ternary-color)"
-          stroke-width="2"
+          d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15"
+          stroke="var(--ternary-color)"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
-          d="M5 16L5 17C5 18.1046 5.89543 19 7 19L17 19C18.1046 19 19 18.1046 19 17V16"
+          d="M12 3V16M12 16L16.5 10M12 16L7.5 10"
           stroke="var(--ternary-color)"
-          stroke-width="2"
+          stroke-width="1.5"
         />
       </svg>
     </label>
@@ -271,21 +277,23 @@
       title="Import nodes from JSON file"
     >
       <svg
-        width="30px"
-        height="30px"
+        width="25px"
+        height="25px"
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          d="M12 14L11.2929 14.7071L12 15.4142L12.7071 14.7071L12 14ZM13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44771 11 5L13 5ZM6.29289 9.70711L11.2929 14.7071L12.7071 13.2929L7.70711 8.29289L6.29289 9.70711ZM12.7071 14.7071L17.7071 9.70711L16.2929 8.29289L11.2929 13.2929L12.7071 14.7071ZM13 14L13 5L11 5L11 14L13 14Z"
-          fill="var(--ternary-color)"
-          stroke-width="2"
+          d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15"
+          stroke="var(--ternary-color)"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         />
         <path
-          d="M5 16L5 17C5 18.1046 5.89543 19 7 19L17 19C18.1046 19 19 18.1046 19 17V16"
+          d="M12 3V16M12 16L16.5 10M12 16L7.5 10"
           stroke="var(--ternary-color)"
-          stroke-width="2"
+          stroke-width="1.5"
         />
       </svg>
     </label>
