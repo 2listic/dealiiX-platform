a9566bda55f303a9fd06668eb45e3f820422df22
Pablo Colturi <pcolturi@proton.me>
2025-08-28 17:50:24 +0200

    #69 use localstorage to make color mode persistent

--- src/lib/stores/colorModeStore.svelte.ts
+++ src/lib/stores/colorModeStore.svelte.ts
@@ -1,6 +1,33 @@
 import { type ColorModeClass } from '@xyflow/svelte'
 
-let colorMode: ColorModeClass = $state('light')
+// Check localStorage for initial value
+const initialColorMode =
+  (localStorage.getItem('colorMode') as ColorModeClass) || 'light'
+
+// Initialize reactive state
+let colorMode: ColorModeClass = $state(initialColorMode)
+
+/**
+ * It updates the theme mode browser side and in the electron main process if running. It also updates the localStorage
+ */
+const updateThemeMode = async () => {
+  // @ts-ignore
+  if (window.electron) {
+    // @ts-ignore
+    const actualTheme = await window.electron.invoke('set-theme', colorMode)
+    console.log('Electron theme: ', actualTheme)
+  }
+  if (colorMode == 'dark') {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+  localStorage.setItem('colorMode', colorMode)
+  console.log('Browser theme', colorMode)
+}
+
+// Initialize colore mode
+updateThemeMode()
 
 export const colorModeState = {
   get value() {
@@ -8,8 +35,11 @@ export const colorModeState = {
   },
   set value(value) {
     colorMode = value
+    updateThemeMode()
   },
   toggle() {
-    colorMode = colorMode === 'light' ? 'dark' : 'light'
+    const newColorMode = colorMode === 'light' ? 'dark' : 'light'
+    colorMode = newColorMode
+    updateThemeMode()
   },
 }
