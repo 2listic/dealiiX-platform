96b41c5a761995a69e37b25be2a56b458c06a833
Pablo Colturi <pcolturi@proton.me>
2025-08-29 17:28:59 +0200

    refactor sidebar buttons icons into components

---
+++ src/lib/components/icons/ImportIcon.svelte
@@ -0,0 +1,24 @@
+<script>
+  let { width, height } = $props()
+</script>
+
+<svg
+  {width}
+  {height}
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15"
+    stroke="var(--ternary-color)"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M12 3V16M12 16L16.5 10M12 16L7.5 10"
+    stroke="var(--ternary-color)"
+    stroke-width="1.5"
+  />
+</svg>
