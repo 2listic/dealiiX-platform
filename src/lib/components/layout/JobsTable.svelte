aea603b13798a327aab281ceb5e48a73ba7064fd
Pablo Colturi <pcolturi@proton.me>
2025-09-24 16:38:48 +0200

    #80 add transitions + css + onmount get jobs list

--- src/lib/components/layout/JobsTable.svelte
+++ src/lib/components/layout/JobsTable.svelte
@@ -1,6 +1,10 @@
 <script>
+  import { onMount } from 'svelte'
   import { jobsState, jobsListState } from '../../stores/jobsStore.svelte'
   import { getJobsState } from '../../utils/sshMessages'
+  import { fade, slide } from 'svelte/transition'
+
+  onMount(() => getJobsState())
 
   const toggleExpand = () => {
     jobsListState.toggle()
@@ -18,53 +22,60 @@
 </button>
 <div id="container-table-jobs">
   {#if jobsListState.isExpanded}
-    <table>
-      <thead>
-        <tr>
-          {#each jobsState.current[0] as headCell, i (i)}
-            <td>{headCell}</td>
+    <div transition:slide>
+      <table id="table-jobslist" transition:fade>
+        <thead>
+          <tr>
+            {#each jobsState.current[0] as headCell, i (i)}
+              <td>{headCell}</td>
+            {/each}
+          </tr>
+        </thead>
+        <tbody>
+          {#each jobsState.current as line, index (line[0])}
+            {#if index > 0}
+              <tr>
+                {#each line as bodyCell, i (i)}
+                  <td>{bodyCell}</td>
+                {/each}
+              </tr>
+            {/if}
           {/each}
-        </tr>
-      </thead>
-      <tbody>
-        {#each jobsState.current as line, index (line[0])}
-          {#if index > 0}
-            <tr>
-              {#each line as bodyCell, i (i)}
-                <td>{bodyCell}</td>
-              {/each}
-            </tr>
-          {/if}
-        {/each}
-      </tbody>
-    </table>
-  {:else}
-    <!-- <div>-</div> -->
+        </tbody>
+      </table>
+    </div>
   {/if}
 </div>
 
 <style>
   #joblist-title {
     font-size: 0.8em;
+    font-weight: bold;
     margin-right: 0.5vh;
   }
 
   #container-table-jobs {
+    width: 40vw;
     max-height: 50vh;
     overflow-y: auto;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    padding: 1vh;
-    margin-top: 1vh;
+    overflow-x: hidden;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
     background-color: var(--primary-color);
+  }
+
+  #table-jobslist {
+    margin: 1vw;
+    width: 100%;
+    table-layout: auto; /* forces equal column distribution */
   }
 
   #button-jobslist-expansion {
     color: var(--ternary-color);
     background-color: var(--background-color-secondary);
+    min-width: 2em;
+    min-height: 2em;
     border: 1px solid grey;
-    border-radius: 5px;
+    border-radius: 10px;
     cursor: pointer;
   }
   #button-jobslist-expansion:hover {
