66ec0f816b432d9a9954a25245df06aae33b9339
Pablo Colturi <pcolturi@proton.me>
2026-02-09 09:28:26 +0100

    fix docs

--- src/lib/types/executionStatus.ts
+++ src/lib/types/executionStatus.ts
@@ -1,6 +1,6 @@
 /**
- * Raw status values written by CORAL as touch file extensions
- * (e.g. "123.running", "123.succeeded", "456.failed")
+ * Raw node's status values written by CORAL Backend as touch file extensions
+ * (e.g. "123.running", "123.succeeded", "456.failed" where 123 and 456 are the node IDs)
  */
 export enum ExecNodeStatus {
   FAILED = 'failed',
